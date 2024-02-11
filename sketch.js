a12555bb8266c3990d50166b70640980a859cf3e
vaibhavyxx <vd5443@rit.edu>
2024-02-11 02:42:43 -0500

    issue w making recursive patterns

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup(){
+    createCanvas(400,400);
+}
+
+function draw(){
+    let counter=0;
+    background(117, 172, 255);
+    makeCircles(200, 200, 0);
+}
+
+function makeCircles(x,y, counter){
+//issue w recursion
+}
