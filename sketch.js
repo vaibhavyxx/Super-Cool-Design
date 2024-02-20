ae2a0449d9480340422b9e3664f08262fc0f14ba
vaibhavyxx <vd5443@rit.edu>
2024-02-20 17:35:07 -0500

    made a recursive pattern

--- sketch.js
+++ sketch.js
@@ -1,18 +1,26 @@
+let app;
 function setup(){
-    createCanvas(800,800);
-    background(75, 125, 219);
+    app = createCanvas(800,800);
+    app.parent("#sketch1");
+    background(0);
 }
 
-function draw(){
-    makeCircles(100, 100, 100);
+//Used a mix of recursive pattern and for loop to make
+//the code run faster and be computationally good
+function draw(){    
+    for(let y=0; y <850; y+=100){
+        makeCircles(0, y, 100,235, 91, 118,40);
+        makeCircles(-25,y, 100, 68, 74, 242, 30);
+        makeCircles(-50,y, 100, 61, 81, 191, 25);
+        makeCircles(10,y, 100, 247, 249, 255, 7);
+    }
 }
 
-function makeCircles(x, y, r){
-//issue w recursion
+function makeCircles(x, y, r, R, G, B, Alpha){
     circle(x,y,r);
-    fill(235, 91, 118);
-    
-    if(r > 5){
-        makeCircles(x+r, y+r, r/2)
+    fill(R, G, B, Alpha);
+    noStroke();
+    if(x < 800){
+        makeCircles(x+1.5*r, y, r)
     }
 }
